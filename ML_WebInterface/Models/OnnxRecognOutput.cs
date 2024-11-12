e5d49814d5c13c546752131fd7b7f07fc587fb25
Kurehe <artem.kazak002@gmail.com>
2024-11-12 10:25:32 +0300

    мелкие правки

--- ML_WebInterface/Models/OnnxRecognOutput.cs
+++ ML_WebInterface/Models/OnnxRecognOutput.cs
@@ -5,6 +5,7 @@ namespace ML_WebInterface.Models
     public class OnnxRecognOutput
     {
         [ColumnName("OutPut10x1")]  // эт название выходного слоя
+        [VectorType(10)]
         public float[] Score;       // массив на 10 элементов (0 - 9).
     }
 }
